6b2fa79b13240211b63c4a000463f49e088dc1a6
seanoshea <oshea.ie@gmail.com>
2011-10-03 23:25:51 -0700

    Adding some sproutcore boilerplate. Need to flesh out the kt main app view controller. Still trying to figure out if there's a good way to allow for sproutcore to integrate into a HTML page without having to write the whole HTML page in handlebars.

---
+++ js/kt/apps/kt/kt.js
@@ -0,0 +1,8 @@
+Kt = SC.Application.create();
+
+SC.ready(function() {
+    Kt.mainPane = SC.TemplatePane.append({
+        layerId: 'kt',
+        templateName: 'kt'
+    });
+});
---
+++ js/kt/apps/kt/tests/kt_test.js
@@ -0,0 +1,9 @@
+module("Kt");
+
+// TODO: Replace with real unit test
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
