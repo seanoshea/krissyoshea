e361870a5255ae56ee3def00bac807d007fc6a5a
seanoshea <oshea.ie@gmail.com>
2012-10-12 21:38:28 -0700

    Moving to python 2.7

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import wsgiref.handlers
 
-from google.appengine.ext import webapp
+import webapp2
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
@@ -9,15 +9,15 @@ import logging
 from django.utils import simplejson
 from models import Portfolio
  
-class MainHandler(webapp.RequestHandler):
- 
+class MainHandler(webapp2.RequestHandler):
+
   def get(self):
     template_values = {
         'url': self.request.uri
     }
     self.response.out.write(template.render("index.html", template_values))
 
-class ApiHandler(webapp.RequestHandler):
+class ApiHandler(webapp2.RequestHandler):
  
   def get(self):
     # check out the API request
@@ -43,31 +43,24 @@ class ApiHandler(webapp.RequestHandler):
       logging.info(error)
       return error
 
-def main():
+app = webapp2.WSGIApplication([('/', MainHandler), ('/api', ApiHandler)], debug=True)
 
-  # not sure if this is the best place to put the portfolio initialization logic - seems to work fine though
-  query = Portfolio.all()
-  portfolios = query.count()
-  if portfolios == 0:
-      # time to put the portfolios in the data store
-      logging.info('Creating the Portfolios')
-      foodPortfolio = Portfolio(name="Food",numberOfImages=10,startIndex=0, orderIndex=0);
-      propsPortfolio = Portfolio(name="Props",numberOfImages=28,startIndex=0, orderIndex=1);
-      flowersPortfolio = Portfolio(name="Flowers",numberOfImages=13,startIndex=0, orderIndex=2);
-      interiorsPortfolio = Portfolio(name="Interiors",numberOfImages=3,startIndex=0, orderIndex=3);
-      foodPortfolio.put()
-      propsPortfolio.put()
-      flowersPortfolio.put()
-      interiorsPortfolio.put()
-  else:
-      # keep deleting the portfolios every second time the user visits the page for now.
-      # completely unnecessary control flow, but it's just a handy way of clearning out the portfolios for now.
-      logging.info('Deleting the Portfolios')
-      db.delete(query.run())
-
-  # check to see whether the request is trying to hit the api or not
-  application = webapp.WSGIApplication([('/', MainHandler), ('/api', ApiHandler)], debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
- 
-if __name__ == '__main__':
-  main()
+# not sure if this is the best place to put the portfolio initialization logic - seems to work fine though
+query = Portfolio.all()
+portfolios = query.count()
+if portfolios == 0:
+    # time to put the portfolios in the data store
+    logging.info('Creating the Portfolios')
+    foodPortfolio = Portfolio(name="Food",numberOfImages=10,startIndex=0, orderIndex=0);
+    propsPortfolio = Portfolio(name="Props",numberOfImages=28,startIndex=0, orderIndex=1);
+    flowersPortfolio = Portfolio(name="Flowers",numberOfImages=13,startIndex=0, orderIndex=2);
+    interiorsPortfolio = Portfolio(name="Interiors",numberOfImages=3,startIndex=0, orderIndex=3);
+    foodPortfolio.put()
+    propsPortfolio.put()
+    flowersPortfolio.put()
+    interiorsPortfolio.put()
+else:
+    # keep deleting the portfolios every second time the user visits the page for now.
+    # completely unnecessary control flow, but it's just a handy way of clearning out the portfolios for now.
+    logging.info('Deleting the Portfolios')
+    db.delete(query.run())
