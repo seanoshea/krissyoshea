dd83d761decc291062b81db4a7246b3209eca9e7
seanoshea <oshea.ie@gmail.com>
2012-10-13 13:13:59 -0700

    Handling errors when there are no portfolios.

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ import logging
 
 from django.utils import simplejson
 from models import Portfolio
- 
+
+# not sure if this is the best place to put the portfolio initialization logic - seems to work fine though
+query = Portfolio.all()
+portfolios = query.count()
+if portfolios == 0:
+    # time to put the portfolios in the data store
+    logging.info('Creating the Portfolios')
+    foodPortfolio = Portfolio(name="Food",numberOfImages=10,startIndex=0, orderIndex=0);
+    propsPortfolio = Portfolio(name="Props",numberOfImages=28,startIndex=0, orderIndex=1);
+    flowersPortfolio = Portfolio(name="Flowers",numberOfImages=13,startIndex=0, orderIndex=2);
+    interiorsPortfolio = Portfolio(name="Interiors",numberOfImages=3,startIndex=0, orderIndex=3);
+    foodPortfolio.put()
+    propsPortfolio.put()
+    flowersPortfolio.put()
+    interiorsPortfolio.put()
+else:
+    # keep deleting the portfolios every second time the user visits the page for now.
+    # completely unnecessary control flow, but it's just a handy way of clearning out the portfolios for now.
+    logging.info('Deleting the Portfolios')
+    db.delete(query.run())
+
 class MainHandler(webapp2.RequestHandler):
 
   def get(self):
@@ -31,36 +51,16 @@ class ApiHandler(webapp2.RequestHandler):
         if portfoliosQuery.count() != 0:
             apiResponse = simplejson.dumps([p.to_dict() for p in portfoliosQuery.run()])
         else :
-            apiResponse = self.generateError('There are no portfolios')
+            apiResponse = self.generateError('There are no portfolios', '2')
     else :
         # only portfolios available at this stage - should probably send back some kind of error here
-        apiResponse = self.generateError('Invalid API request')
+        apiResponse = self.generateError('Invalid API request', '1')
     self.response.headers['Content-Type'] = 'application/json;charset=utf-8'
+    self.response.headers['X-KT-API'] = 'X-KT-API'
     self.response.out.write(apiResponse)
     
-  def generateError(self, error):
-      # TODO - write this error to the response stream.
+  def generateError(self, error, code):
       logging.info(error)
-      return error
+      return simplejson.dumps({'error': {'code': code, 'description': error}})
 
 app = webapp2.WSGIApplication([('/', MainHandler), ('/api', ApiHandler)], debug=True)
-
-# not sure if this is the best place to put the portfolio initialization logic - seems to work fine though
-query = Portfolio.all()
-portfolios = query.count()
-if portfolios == 0:
-    # time to put the portfolios in the data store
-    logging.info('Creating the Portfolios')
-    foodPortfolio = Portfolio(name="Food",numberOfImages=10,startIndex=0, orderIndex=0);
-    propsPortfolio = Portfolio(name="Props",numberOfImages=28,startIndex=0, orderIndex=1);
-    flowersPortfolio = Portfolio(name="Flowers",numberOfImages=13,startIndex=0, orderIndex=2);
-    interiorsPortfolio = Portfolio(name="Interiors",numberOfImages=3,startIndex=0, orderIndex=3);
-    foodPortfolio.put()
-    propsPortfolio.put()
-    flowersPortfolio.put()
-    interiorsPortfolio.put()
-else:
-    # keep deleting the portfolios every second time the user visits the page for now.
-    # completely unnecessary control flow, but it's just a handy way of clearning out the portfolios for now.
-    logging.info('Deleting the Portfolios')
-    db.delete(query.run())
