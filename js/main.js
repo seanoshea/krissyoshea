07eeafa618ddc1711a3857898119723d08ea717e
seanoshea <oshea.ie@gmail.com>
2013-12-31 18:44:51 +0000

    LICENSE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2012 - 2013 Sean O' Shea
+    Copyright 2012 - 2014 Sean O' Shea
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
