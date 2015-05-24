b2643ede0341b25a733d3fd96f9ef87a52f572ce
seanoshea <oshea.ie@gmail.com>
2015-05-24 08:49:53 +0100

    A few more jsonp tests.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,10 +7,9 @@ angular.module('krissyosheaApp')
 	$scope.errorMessage = 0;
 	var loadPortfolios = function() {
 		$scope.loading = 1;
-		$http.jsonp('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&user_id=91622522@N07&api_key=3426649638b25fe317be122d3fbbc1b1&format=json&jsoncallback=JSON_CALLBACK')
-			.success(function(data) {
-  				$scope.portfolios = data.photosets.photoset;
-				$scope.loading = 0;
+		$http.jsonp('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&user_id=91622522@N07&api_key=3426649638b25fe317be122d3fbbc1b1&format=json&jsoncallback=JSON_CALLBACK').success(function(data) {
+			$scope.portfolios = data.photosets.photoset;
+			$scope.loading = 0;
 	    }).error(function() {
     	  $scope.errorMessage = 'There was an error when loading the portfolios';
     	});
