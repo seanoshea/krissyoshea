edf900ec74c2819ed39ff2c610d7231cbc415e2a
seanoshea <oshea.ie@gmail.com>
2015-05-25 10:18:56 +0100

    Some more unit tests for loading the portfolio details. Still needs some error handling.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,12 @@ angular.module('krissyosheaApp')
   .controller('MainCtrl', function ($scope, portfolioService) {
 	$scope.loading = 0;
 	portfolioService.get();
-	$scope.$on('portfolioLoadedMessage',function(){
+	var randomiseHomeScreenImage = function() {
+
+	};
+	$scope.$on('portfolioLoadedMessage',function() {
 		$scope.loading = 0;
+		randomiseHomeScreenImage();
 	});
+
   });
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -4,17 +4,38 @@
 
 angular.module('krissyosheaApp.services', []).factory('portfolioService', function ($rootScope, $http) {
     return {
-    	portfolioLoadedMessage: 'portfolioLoadedMessage',
-    	portfolioLoadedFailedMessage: 'portfolioLoadedFailedMessage',
+    	portfolioListLoadedMessage: 'portfolioListLoadedMessage',
+        portfolioDetailsLoadedMessage: 'portfolioDetailsLoadedMessage',
         portfolios: {},
+        portfolioDetails: [],
         get: function() {
             var self = this;
 			$http.jsonp('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&user_id=91622522@N07&api_key=3426649638b25fe317be122d3fbbc1b1&format=json&jsoncallback=JSON_CALLBACK').success(function(data) {
 				self.portfolios = data.photosets.photoset;
+                self.getPortfolioDetails(self.portfolios);
 				$rootScope.$broadcast(self.portfolioLoadedMessage);
 	    	}).error(function() {
-	    	  $rootScope.$broadcast(self.portfolioLoadedFailedMessage);
+	    	  
     		});
-        }
+        },
+        getPortfolioDetails: function(portfolios) {
+            angular.forEach(portfolios, function(value, key) {
+                this.getPortfolio(value.id);
+            }, this);
+        },
+        getPortfolio: function(portfolioId) {
+            var self = this;
+            $http.jsonp('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&extras=url_sq,url_s,url_m,url_o&photoset_id=' + portfolioId + '&api_key=3426649638b25fe317be122d3fbbc1b1&format=json&jsoncallback=JSON_CALLBACK').success(function(data) {
+                self.addPortfolioDetails(data)
+            }).error(function() {
+
+            });
+        },
+        addPortfolioDetails: (function(data) {
+            this.portfolioDetails.push(data);
+            if (this.portfolioDetails.length == this.portfolios.length) {
+                $rootScope.$broadcast(self.portfolioDetailsLoadedMessage);
+            }
+        })
     };
 });
