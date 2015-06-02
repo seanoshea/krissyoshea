e319046aac21f74c462483ed115cd61b1521c427
seanoshea <oshea.ie@gmail.com>
2015-06-02 07:40:51 +0100

    Changes to how the main portfolio is loaded on the home screen.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,14 @@
 
 angular.module('krissyosheaApp')
   .controller('MainCtrl', function ($scope, $location, portfolioService) {
-	$scope.loading = 0;
+	$scope.loading = 1;
 	$scope.randomizedPortfolioKey = 0;
 	$scope.mainScreenImagePressed = function() {
         $location.search({'portfolioId': $scope.randomizedPortfolioKey.id});
 		$location.path('/portfolio');
 	};
-	var randomiseMainScreenImage = function() {
-		var index = Math.floor(Math.random() * portfolioService.portfolios.length - 1), url;
+	$scope.randomiseMainScreenImage = function() {
+		var index = Math.floor(Math.random() * portfolioService.portfolios.length), url;
 		angular.forEach(portfolioService.portfolios, function(value, key) {
 			if (key === index) {
 				var portfolioDetails = portfolioService.portfolioDetailsWithPortfolioId(value.id);
@@ -25,8 +25,13 @@ angular.module('krissyosheaApp')
 		}, this);
 		return url;
 	};
-	$scope.$on(portfolioService.portfolioDetailsLoadedMessage, function() {
-		$scope.loading = 0;
-		$scope.mainScreenImage = randomiseMainScreenImage();
-	});
+	if (!portfolioService.portfoliosLoaded) {
+		$scope.$on(portfolioService.portfolioDetailsLoadedMessage, function() {
+			$scope.loading = 0;
+			var url = $scope.randomiseMainScreenImage();
+			$scope.mainScreenImage = url;
+		});
+	} else {
+		// TODO: Need to load the randomized image from shared state.
+	}
   });
--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -13,8 +13,14 @@ angular.module('krissyosheaApp')
 	$scope.portfolioMenuItemPressed = function(portfolio) {
         $location.search({'portfolioId': portfolio.id});
 		$location.path('/portfolio');
+		$scope.portfolioMenuPressed();
 	};
 	$scope.$on(portfolioService.portfolioDetailsLoadedMessage,function() {
 		$scope.portfolios = portfolioService.portfolios;
+	});
+	$scope.$on('$locationChangeStart', function(event, next, current) {
+		if ($scope.portfolioMenuVisible && next.indexOf('portfolio') === -1) {
+			$scope.portfolioMenuPressed();
+		}
 	});
   });
