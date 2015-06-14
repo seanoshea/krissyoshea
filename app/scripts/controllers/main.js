54d72e9647bfe19b1934e79fc14aa615ed5c83e6
seanoshea <oshea.ie@gmail.com>
2015-06-14 14:00:08 +0100

    Dealing with the possibility of a primary photo not being set on one of the photo sets.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,17 +10,21 @@ angular.module('krissyosheaApp')
 	};
 	$scope.randomiseMainScreenImage = function() {
 		var index = Math.floor(Math.random() * portfolioService.portfolios.length), url;
-		angular.forEach(portfolioService.portfolios, function(value, key) {
-			if (key === index) {
-				var portfolioDetails = portfolioService.portfolioDetailsWithPortfolioId(value.id);
+		angular.forEach(portfolioService.portfolios, function(portfolioValue, portfolioKey) {
+			if (portfolioKey === index) {
+				var portfolioDetails = portfolioService.portfolioDetailsWithPortfolioId(portfolioValue.id);
 				if (portfolioDetails) {
 					angular.forEach(portfolioDetails.photoset.photo, function(value, key) {
 						if (parseInt(value.isprimary, 10) === 1) {
 							url = value.url_o;
 						}
 					});
-					$scope.randomizedPortfolioKey = value;
-					$window.localStorage.setItem('randomizedPortfolioKey', value);
+					// just in case a portfolio has been set up which does not have a primray photo
+					if (!url) {
+						url = portfolioDetails.photoset.photo[0];
+					}
+					$scope.randomizedPortfolioKey = portfolioValue;
+					$window.localStorage.setItem('randomizedPortfolioKey', portfolioValue);
 					$window.localStorage.setItem('randomizedPortfolioUrl', url);
 				}
 			}
