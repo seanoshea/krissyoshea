9268b1aed0a9e87375f62e5f43510b630b74ca6b
seanoshea <oshea.ie@gmail.com>
2016-03-07 21:37:45 -0500

    Per issue #21 - Images and max width

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -5,7 +5,9 @@
 angular.module('krissyosheaApp.directives', []).directive('responsive', function () {
     return {
         link: function(scope, element, attrs) {
-            var dimension = window.screen.width > 320 ? 'url_o' : 'url_m';
+        	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width
+			xAxis = parseInt(scope.photo.width_o, 10),
+			dimension = width >= xAxis ? 'url_o' : 'url_m';
             element[0].src = scope.photo[dimension];
         }
     };
