d6d8848cfc1d32cbb028a668e690492571afdb95
Shawn Lobo <shawnlobo96@users.noreply.github.com>
2020-05-30 01:08:31 +0530

    Fixed script to run on Browserstack (#52)
    Co-authored-by: Shawn Lobo <shawn@Shawns-MacBook-Pro.local>

--- client/e2e/parallel.conf.js
+++ client/e2e/parallel.conf.js
@@ -1,27 +1,34 @@
-const config = require('./protractor.conf').config;
+// const config = require('./protractor.conf').config;
+config = {}
+config.browserstackUser = process.env.BROWSERSTACK_USERNAME
+config.browserstackKey = process.env.BROWSERSTACK_ACCESS_KEY
+
+config.specs = [
+  './src/**/*.e2e-spec.ts'
+]
 
 config.commonCapabilities = {
-  'browserstack.user': 'seanoshea4',
-  'browserstack.key': 'd75JMR6aXSRUDXsEqWsX',
+  'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
   'build': 'protractor-browserstack',
   'name': 'parallel_test',
   'browserstack.debug': 'true',
-  'browserName': 'Chrome'
+  'browserstack.local': 'true'
 };
 config.directConnect = false;
 config.multiCapabilities = [{
   'browserName': 'Chrome'
-},{
+}, {
   'browserName': 'Safari'
-},{
+}, {
   'browserName': 'Firefox'
-},{
+}, {
   'browserName': 'IE'
 }];
 
 // Code to support common capabilities
-config.multiCapabilities.forEach(function(caps){
-  for(var i in config.commonCapabilities) caps[i] = caps[i] || config.commonCapabilities[i];
+config.multiCapabilities.forEach(function (caps) {
+  for (var i in config.commonCapabilities) caps[i] = caps[i] || config.commonCapabilities[i];
 });
 
 exports.config = config;
